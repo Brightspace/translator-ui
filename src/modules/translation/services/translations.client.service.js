d774799c660bb4708e40a98c6a2be6e86d63bacf
Noel Stonehouse <Noel.Stonehouse@D2L.com>
2015-10-19 15:13:15 -0400

    Remove backendUrl injection, and use the envify variable directly instead.

--- src/modules/translation/services/translations.client.service.js
+++ src/modules/translation/services/translations.client.service.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('translation')
-	.factory('Translations', ['$resource', 'backendUrl', function($resource, backendUrl) {
+	.factory('Translations', ['$resource', function($resource) {
 		return $resource(
-			backendUrl + 'translations/:tokenId',
-			{ tokenId: '@_id' }
+			process.env.BACKEND_URL + '/translations/:tokenId/:action',
+			{
+				tokenId: '@id',
+				action: '@action'
+			}
 		);
-	}])
-	.value('backendUrl', 'http://192.168.99.100:49160/');
+	}]);
